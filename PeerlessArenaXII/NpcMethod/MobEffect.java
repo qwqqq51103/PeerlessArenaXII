374bb3744f04d67382f84c37a0a5e1f68dd0d7ae
Tara <>
2022-06-07 11:39:37 +0800

    0607 Test

--- PeerlessArenaXII/NpcMethod/MobEffect.java
+++ PeerlessArenaXII/NpcMethod/MobEffect.java
@@ -29,7 +29,7 @@ public class MobEffect {
         switch (num) {
             case 0:
                 //虛弱
-                cdv.STR = cdv.STR / 2;
+                cdv.STR = cdv.STR / 21;
                 a = "此回合不幸獲得 虛弱 狀態";
                 printfChatLog(a, 3);
                 break;
